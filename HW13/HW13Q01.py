a1b398485bf42f793e0d4355c9df363e1cfeada4
GreatWolf155 <ygreenfield484@gmail.com>
2026-02-20 12:26:15 +0200

    Homework 13 Completed

---
+++ HW13/HW13Q01.py
@@ -0,0 +1,38 @@
+"""
+Part A
+For each question: What is the missing operation (?)
+
+Example:
+
+{1, 2} ? {3, 4} = {1, 2, 3, 4}
+
+Answer: | , because-
+
+{1, 2} | {3, 4} = {1, 2, 3, 4}
+
+Solve:
+
+{1, 2} ? {3, 4} = {1, 2, 3, 4}
+Answer: |
+
+{1, 2, 3} ? {3, 4} = {3}
+Answer: &
+
+{1, 2, 3, 4} ? {3, 4} = {1, 2}
+Answer: ^
+
+{1, 2, 3} ? {3, 4} = {1, 2, 4}
+Answer: ^
+
+{1, 2, 3, 4} ? {1, 2} = True
+Answer: > or >=
+
+{1, 2} ? {1, 2, 3, 4} = True
+Answer: < or <=
+
+{5, 6} ? {6, 7} = {5, 6, 7}
+Answer: |
+
+{10, 11, 12} ? {12, 13} = {10, 11}
+Answer: -
+"""
