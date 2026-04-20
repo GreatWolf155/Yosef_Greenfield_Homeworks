850d1be933a043ee4c9938ff5f3b095682b09121
GreatWolf155 <ygreenfield484@gmail.com>
2026-04-20 23:00:45 +0300

    HW22 Completed

--- HW22/HW22 - SQL Q03 Netflix.sql
+++ HW22/HW22 - SQL Q03 Netflix.sql
@@ -1,10 +1,9 @@
 -- 1
 SELECT DISTINCT type
-FROM netflix_movies_detailed_up_to_2025
-ORDER BY type;
+FROM netflix_movies_detailed_up_to_2025;
 
 -- 2
-SELECT *
+SELECT DISTINCT *
 FROM netflix_movies_detailed_up_to_2025
 WHERE "cast" IS NOT NULL AND genres LIKE '%Action%'
 LIMIT 20;
@@ -12,28 +11,27 @@ LIMIT 20;
 -- 3
 SELECT *
 FROM netflix_movies_detailed_up_to_2025
-WHERE release_year = 2015 AND genres LIKE '%Action%'
+WHERE release_year > 2015 AND genres LIKE '%Action%'
 LIMIT 5;
 
 -- 4
--- assuming you mean sort the previous question by highest rating first
 SELECT *
 FROM netflix_movies_detailed_up_to_2025
-WHERE release_year = 2015 AND genres LIKE '%Action%'
+WHERE release_year > 2015 AND genres LIKE '%Action%'
 ORDER BY rating DESC
 LIMIT 5;
 
 -- 5
--- Again, assuming the request is for the previous query
 SELECT
 	title as movie,
 	rating as score
 FROM netflix_movies_detailed_up_to_2025
-WHERE release_year = 2015 AND genres LIKE '%Action%'
+WHERE release_year > 2015 AND genres LIKE '%Action%'
 ORDER BY rating DESC
 LIMIT 5;
 
 -- 6 Bonus
+-- SELECT title, "cast"
 SELECT *
 FROM netflix_movies_detailed_up_to_2025
 WHERE "cast" IS NULL;
