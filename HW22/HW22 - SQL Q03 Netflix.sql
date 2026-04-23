21f0bac01f65788d99e16677b7a978f111adf10c
GreatWolf155 <ygreenfield484@gmail.com>
2026-04-23 17:29:06 +0300

    HW22 Q3 Updated

--- HW22/HW22 - SQL Q03 Netflix.sql
+++ HW22/HW22 - SQL Q03 Netflix.sql
@@ -3,22 +3,26 @@ SELECT DISTINCT type
 FROM netflix_movies_detailed_up_to_2025;
 
 -- 2
-SELECT DISTINCT *
+SELECT DISTINCT "cast"
 FROM netflix_movies_detailed_up_to_2025
-WHERE "cast" IS NOT NULL AND genres LIKE '%Action%'
+WHERE genres LIKE '%Action%' AND "cast" IS NOT NULL
+    AND TRIM("cast") != ''
 LIMIT 20;
 
 -- 3
 SELECT *
 FROM netflix_movies_detailed_up_to_2025
-WHERE release_year > 2015 AND genres LIKE '%Action%'
+WHERE release_year > 2015
+    AND genres LIKE '%Action%'
+    AND type = 'Movie'
 LIMIT 5;
 
 -- 4
 SELECT *
 FROM netflix_movies_detailed_up_to_2025
-WHERE release_year > 2015 AND genres LIKE '%Action%'
-ORDER BY rating DESC
+WHERE release_year > 2015
+    AND genres LIKE '%Action%'
+    AND type = 'Movie'ORDER BY rating DESC
 LIMIT 5;
 
 -- 5
@@ -26,12 +30,14 @@ SELECT
 	title as movie,
 	rating as score
 FROM netflix_movies_detailed_up_to_2025
-WHERE release_year > 2015 AND genres LIKE '%Action%'
+WHERE release_year > 2015
+    AND genres LIKE '%Action%'
+    AND type = 'Movie'
 ORDER BY rating DESC
 LIMIT 5;
 
 -- 6 Bonus
 -- SELECT title, "cast"
-SELECT *
+SELECT title, release_year
 FROM netflix_movies_detailed_up_to_2025
-WHERE "cast" IS NULL;
+WHERE ("cast" IS NULL OR TRIM("cast") = '');
